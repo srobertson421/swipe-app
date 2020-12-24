7e133be4d2cf37985639dd95df49872182150e67
Sean Robertson <srobertson421@gmail.com>
2020-12-23 18:57:55 -0800

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,47 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import Swiper, { SwiperPluginLazyload } from 'tiny-swiper';
 
-import confetti from 'canvas-confetti';
+Swiper.use([ SwiperPluginLazyload ]);
 
-confetti.create(document.getElementById('canvas'), {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+const swiper = new Swiper('#app', {
+  loop: true
+});
+
+function createImage(url) {
+  const newImg = new Image();
+  newImg.src = url;
+  return newImg;
+}
+
+const navLinkEls = document.querySelectorAll('.nav-link');
+const navIcons = [
+  createImage('https://icongr.am/entypo/home.svg?size=35&color=FFFFFF'),
+  createImage('https://icongr.am/entypo/user.svg?size=35&color=FFFFFF'),
+  createImage('https://icongr.am/entypo/cog.svg?size=35&color=FFFFFF')
+];
+const activeNavIcons = [
+  createImage('https://icongr.am/entypo/home.svg?size=35&color=7FFFD4'),
+  createImage('https://icongr.am/entypo/user.svg?size=35&color=8A2BE2'),
+  createImage('https://icongr.am/entypo/cog.svg?size=35&color=FF7F50')
+];
+
+navLinkEls.forEach((el, index) => {
+  el.addEventListener('click', () => {
+    swiper.slideTo(index);
+  });
+});
+
+function setActiveView(currentView) {
+  navLinkEls.forEach((el, index) => {
+    el.classList.remove('active');
+    el.firstElementChild.replaceWith(navIcons[index]);
+  });
+
+  navLinkEls[currentView].classList.add('active');
+  navLinkEls[currentView].firstElementChild.replaceWith(activeNavIcons[currentView]);
+}
+
+setActiveView(swiper.state.index);
+
+swiper.on('before-slide', (currentIndex, state, nextIndex) => {
+  setActiveView(nextIndex);
+});
